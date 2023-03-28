1b17a6e0036c0a4671a6e01794af19aed402681d
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-03-28 03:34:19 -0300

    Atualização Security

---
+++ src/main/java/com/crud/logagames/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.crud.logagames.security;
+
+import com.crud.logagames.model.Usuario;
+import com.crud.logagames.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        Optional<Usuario> usuario = usuarioRepository.findByUsuario(userName);
+
+        if (((Optional<?>) usuario).isPresent())
+            return new UserDetailsImpl(usuario.get());
+        else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+    }
+}
