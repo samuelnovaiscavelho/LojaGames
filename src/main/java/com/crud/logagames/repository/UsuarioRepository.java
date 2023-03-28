c39fea267aaf761bf17f376564d671b0f68b50ca
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-03-28 03:19:35 -0300

    Atualização Security

---
+++ src/main/java/com/crud/logagames/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.crud.logagames.repository;
+
+import com.crud.logagames.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Optional<Usuario> findByUsuario(String usuario);
+}
